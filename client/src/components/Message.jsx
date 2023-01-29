7980cc9f7697977dadec1755cc0cfcf89a529101
joaobernardino77 <joaobernardino77@gmail.com>
2023-01-29 22:52:03 +0000

    robot icon

--- client/src/components/Message.jsx
+++ client/src/components/Message.jsx
@@ -2,6 +2,8 @@ import "./Message.css";
 import BouncerMessageLoader from "../generic/BouncerMessageLoader";
 import Avatar from "@mui/material/Avatar";
 import SmartToyIcon from "@mui/icons-material/SmartToy";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faRobot } from "@fortawesome/free-solid-svg-icons";
 
 const Message = ({
   userType,
@@ -21,10 +23,14 @@ const Message = ({
             <div className="message-text">
               <div style={{ display: "inline-flex" }}>
                 {userType === "bot" ? (
-                  <SmartToyIcon
+                  <FontAwesomeIcon
+                    icon={faRobot}
                     style={{ height: "40px", width: "40px", color: "white" }}
                   />
                 ) : (
+                  /*<SmartToyIcon
+                    style={{ height: "40px", width: "40px", color: "white" }}
+                  />*/
                   <Avatar />
                 )}
               </div>
